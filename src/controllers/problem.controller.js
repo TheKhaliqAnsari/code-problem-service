3f89531643218371e021adb3dcf2ccba5284f9cc
Khaliq Ansari <khaliq.ansari@sarvagram.com>
2024-10-05 17:20:15 +0530

    Feat: Added unimplemented error for api contract.

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -5,25 +5,36 @@ function pingProblem(req, res) {
 }
 
 function addProblem(req, res) {
+    res.status(501).json({
+        message: 'Not implemented'
+    });
 
 }
 
 
 function getProblem(req, res) {
-
+    res.status(501).json({
+        message: 'Not implemented'
+    });
 }
 
 function getProblems(req, res) {
-
+    res.status(501).json({
+        message: 'Not implemented'
+    });
 }
 
 
 function deleteProblem(req, res) {
-
+    res.status(501).json({
+        message: 'Not implemented'
+    });
 }
 
 function updateProblem(req, res) {
-
+    res.status(501).json({
+        message: 'Not implemented'
+    });
 }
 
 
