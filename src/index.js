5933745c5a6e3cd132ab8343c28d670b35308ca4
Khaliq Ansari <khaliq.ansari@sarvagram.com>
2024-10-05 15:32:53 +0530

    Project arch setup.

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const { PORT } = require('./config/server.config');
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
+app.get('/ping', (req, res) => {
+    res.status(200).send({
+        message: "Server is up"
+    })
+})
+
+
+
+app.listen(PORT, () => {
+    console.log('Server is running on: ', PORT)
+})
